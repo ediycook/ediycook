3ab23e3885757ee1456bac69a9c14964e43c3943
ediycook <e.diycook@gmail.com>
2018-04-28 23:09:44 +0200

    init recipe project

---
+++ ediycook-ms-recipe/src/main/java/com/e/diycook/ms/recipe/web/rest/util/HeaderUtil.java
@@ -0,0 +1,45 @@
+package com.e.diycook.ms.recipe.web.rest.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * Utility class for HTTP headers creation.
+ */
+public final class HeaderUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(HeaderUtil.class);
+
+    private static final String APPLICATION_NAME = "edkmsRecipeApp";
+
+    private HeaderUtil() {
+    }
+
+    public static HttpHeaders createAlert(String message, String param) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-edkmsRecipeApp-alert", message);
+        headers.add("X-edkmsRecipeApp-params", param);
+        return headers;
+    }
+
+    public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".created", param);
+    }
+
+    public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".updated", param);
+    }
+
+    public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".deleted", param);
+    }
+
+    public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
+        log.error("Entity processing failed, {}", defaultMessage);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-edkmsRecipeApp-error", "error." + errorKey);
+        headers.add("X-edkmsRecipeApp-params", entityName);
+        return headers;
+    }
+}

873ead3bd99ec2cef844643e359c2463b87ed0ad
ediycook <e.diycook@gmail.com>
2018-04-29 00:56:43 +0200

    integration parentpom

---
+++ ediycook-ms-common/src/main/java/com/e/diycook/ms/common/domain/FlavourTypeEnum.java
@@ -0,0 +1,93 @@
+package com.e.diycook.ms.common.domain;
+
+public enum FlavourTypeEnum {
+
+	/**
+	 * Key:A
+	 */
+	ABRICOT,
+	/**
+	 * Key:B
+	 */
+	/**
+	 * Key:C
+	 */
+	CARAMEL,
+	CHOCOLAT,
+	CITRON,
+	/**
+	 * Key:D
+	 */
+	/**
+	 * Key:E
+	 */
+	/**
+	 * Key:F
+	 */
+	FRAISE,
+	/**
+	 * Key:G
+	 */
+	/**
+	 * Key:H
+	 */
+	/**
+	 * Key:I
+	 */
+	/**
+	 * Key:J
+	 */
+	/**
+	 * Key:K
+	 */
+	/**
+	 * Key:L
+	 */
+	/**
+	 * Key:M
+	 */
+	/**
+	 * Key:N
+	 */
+	/**
+	 * Key:O
+	 */
+	/**
+	 * Key:P
+	 */
+	/**
+	 * Key:Q
+	 */
+	/**
+	 * Key:R
+	 */
+	/**
+	 * Key:S
+	 */
+	/**
+	 * Key:T
+	 */
+	/**
+	 * Key:U
+	 */
+	/**
+	 * Key:V
+	 */
+	VANILLE,
+	/**
+	 * Key:W
+	 */
+	/**
+	 * Key:X
+	 */
+	/**
+	 * Key:Y
+	 */
+	/**
+	 * Key:Z
+	 */
+	
+	/**/
+	UNKNOWN;
+
+}
